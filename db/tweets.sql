cb52be6089cf3d8640af6aca1a54037504fa13ca
Martin <koo.martin@gmail.com>
2017-10-21 02:33:55 -0400

    updated postgres schema, streamer now inserts into postgres DB (local for now)

--- db/tweets.sql
+++ db/tweets.sql
@@ -1,28 +1,30 @@
---Sets/resets the database schemas
+--Sets resets the database schemas
+--Run this with psql
+--Make sure you connect to the correct database within sql (use -d flag to specify)
 
 DROP TABLE IF EXISTS Tweets, Hashtags, Users CASCADE;
 
 CREATE TABLE Users (
-    user_id integer PRIMARY KEY,
+    user_id bigint PRIMARY KEY,
     user_screen_name varchar(50),
     user_location varchar(50),
     user_time_zone varchar(60),
-    user_UTC_offset integer,
     user_followers_count integer,
     user_status_count integer
 );
 
 CREATE TABLE Tweets (
-    id SERIAL PRIMARY KEY,
+    id bigint PRIMARY KEY,
     created_at timestamptz,
     text VARCHAR(140),
-    user_id integer,
-    FOREIGN KEY (user_id) REFERENCES Users (user_id)
+    user_id bigint,
+    FOREIGN KEY (user_id) REFERENCES Users (user_id),
+    retweet_count integer
 );
 
 CREATE TABLE Hashtags (
     text VARCHAR(30),
-    tweet_id integer,
+    tweet_id bigint,
     FOREIGN KEY (tweet_id) REFERENCES Tweets (id)
 );
 
--- python/streamer.py
+++ python/streamer.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from pymongo import MongoClient
+from datetime import datetime
+#from pymongo import MongoClient #For mongo
+import pdb
+import psycopg2 #postgres
 import json
 
 #keys
@@ -13,16 +16,56 @@ consumer_secret = "24swnGemOmfqDMjtpdnNQaRYpaaU9XYFCRaAUKHx0eKuEKzEhm"
 access_token = "899438978625130496-vpxGi7H71MSv81G3TWTdr4mkx44sVMz"
 access_token_secret = "EJVEh5vgDvmomVwL4s9Dnp91O4GxOh3GqIMZ0FxmzCO3d"
 
-client = MongoClient('127.0.0.1:27017')
-db = client.Main
+#client = MongoClient('127.0.0.1:27017')
+#db = client.Main
+conn = psycopg2.connect('dbname=election_ward user=postgres')
+cur = conn.cursor()
+
+def process_tweet(tweet):
+    #Parse desired information and insert tweet
+    tweet_dict = json.loads(tweet)
+    #Check if user exists
+    user_dict = tweet_dict['user']
+    cur.execute('SELECT * FROM Users WHERE user_id = %s', [user_dict['id']])
+    #Insert to user table if user does not exist
+    if len(cur.fetchall()) == 0:
+        cur.execute('INSERT INTO Users \
+                (user_id, \
+                user_screen_name, \
+                user_location, \
+                user_time_zone, \
+                user_followers_count, \
+                user_status_count) \
+                VALUES (%s, %s, %s, %s, %s, %s)', 
+                (user_dict['id'], user_dict['screen_name'], user_dict['location'], user_dict['time_zone'], user_dict['followers_count'], user_dict['statuses_count']))
+
+    #Check if retweet
+    #if 'retweeted_status' in tweet_dict:
+    #Insert tweet
+    cur.execute('INSERT INTO Tweets \
+            (id,\
+            created_at,\
+            text,\
+            user_id)\
+            VALUES (%s, %s, %s, %s)',
+            (tweet_dict['id'], datetime.strptime(tweet_dict['created_at'], '%a %b %d %H:%M:%S %z %Y'), tweet_dict['text'], user_dict['id']))
+
+
+    #Insert hashtags
+    #cur.execute('INSERT INTO Hashtags
+    #        (text,
+    #        tweet_id)
+    #        VALUES (%s, %s)',
+    #        tweet_dict)
+    print(tweet_dict['text'])
+    print('Successfully inserted tweet')
+    
 
 class MyStreamListener(StreamListener):
     def on_data(self, data):
-        insert_item = json.loads(data)
-        print(insert_item['text'])
-        result = db.tweets.insert_one(insert_item)
-        print('Successfully inserted with result: ')
-        print(result)
+        #insert_item = json.loads(data)
+        #result = db.tweets.insert_one(insert_item)
+        process_tweet(data)
         print()
 
 
